1faae13db88105bc97e3fe9b6a07f8b3f13f3aa6
smithetha <smithetha@myvuw.ac.nz>
2017-04-11 11:58:11 +1200

    Update number-printer.cpp
    sg

--- number-printer.cpp
+++ number-printer.cpp
@@ -2,5 +2,6 @@
   int main (){
   int number = 100;
   printf ( "%d" , number );
+    printf ( "%d" , number );printf ( "%d" , number );printf ( "%d" , number );printf ( "%d" , number );printf ( "%d" , number );printf ( "%d" , number );printf ( "%d" , number );printf ( "%d" , number );printf ( "%d" , number );printf ( "%d" , number );
   return 0;
 }
